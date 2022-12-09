77bdd30d1e80ba1a0f8bcd8c8eaafee485d7411c
dadang17 <iskandardzulqornain38@gmail.com>
2022-12-09 11:29:58 +0700

    create app weather

---
+++ src/api.js
@@ -0,0 +1,11 @@
+export const geoApiOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "a9af0c475bmsh0f6d73416b8aa11p152241jsnaa80169ff80e",
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "7c4378621e3b12e29a561fa5be4cac4e";
